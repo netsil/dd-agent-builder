e449ef6a4e6096681c3dadf2c998535901574487
Quentin Madec <quentin.madec@datadoghq.com>
2017-01-11 14:07:37 -0500

    Sync git repository at build time

--- recipes/build.rb
+++ recipes/build.rb
@@ -6,6 +6,19 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+git "#{build_user_home}\\dd-agent-omnibus" do
+  repository 'https://github.com/DataDog/dd-agent-omnibus'
+  revision node['dd-agent-builder']['dd-agent-omnibus_branch']
+  action :sync
+end
+
+git "#{build_user_home}\\integrations-core" do
+  repository 'https://github.com/DataDog/integrations-core'
+  revision node['dd-agent-builder']['dd-integrations-core_branch']
+  action :sync
+end
+
 omnibus_build 'datadog-agent' do
   project_dir "#{build_user_home}\\dd-agent-omnibus"
   log_level :info
--- recipes/prepare.rb
+++ recipes/prepare.rb
@@ -10,18 +10,6 @@
 include_recipe 'omnibus'
 include_recipe 'dd-agent-builder::golang'
 
-git "#{build_user_home}\\dd-agent-omnibus" do
-  repository 'https://github.com/DataDog/dd-agent-omnibus'
-  revision node['dd-agent-builder']['dd-agent-omnibus_branch']
-  action :sync
-end
-
-git "#{build_user_home}\\integrations-core" do
-  repository 'https://github.com/DataDog/integrations-core'
-  revision node['dd-agent-builder']['dd-integrations-core_branch']
-  action :sync
-end
-
 directory node['dd-agent-builder']['install_dir'] do
   action :create
   recursive true
